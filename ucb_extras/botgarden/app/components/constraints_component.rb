ca3aa9f5542d9a7c1f885aa5d28f7671f772cd57
Miriam Campos-Quinn <mcq@berkeley.edu>
2026-07-06 13:15:52 -0700

    botgarden: make some requested changes to advanced search layout

---
+++ ucb_extras/botgarden/app/components/constraints_component.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+class ConstraintsComponent < Blacklight::ConstraintsComponent
+
+  def self.for_advanced_search(**kwargs)
+    new(tag: :dl,
+      render_headers: false,
+      id: nil,
+      classes: 'query row mx-0 my-3 ',
+      query_constraint_component: AdvancedSearchConstraintLayoutComponent,
+      facet_constraint_component_options: { layout: AdvancedSearchConstraintLayoutComponent },
+      start_over_component: nil,
+      **kwargs)
+  end
+
+  def self.for_search_history(**kwargs)
+    new(tag: :dl,
+        render_headers: false,
+        id: nil,
+        classes: 'query row',
+        query_constraint_component: SearchHistoryConstraintLayoutComponent,
+        facet_constraint_component_options: { layout: SearchHistoryConstraintLayoutComponent },
+        start_over_component: nil,
+        **kwargs)
+  end
+
+  def initialize(
+      search_state:,
+      tag: :div,
+      render_headers: true,
+      id: 'appliedParams',
+      classes: 'clearfix constraints-container',
+      query_constraint_component: ConstraintLayoutComponent,
+      query_constraint_component_options: {},
+      facet_constraint_component: ConstraintComponent,
+      facet_constraint_component_options: {},
+      start_over_component: StartOverButtonComponent
+    )
+    @index = 0
+    super
+  end
+
+  def query_constraints
+    if @search_state.query_param.present? || (!@search_state.has_constraints? && is_for_search_history?)
+      query_constraint = render(
+        @query_constraint_component.new(
+          search_state: @search_state,
+          index: @index,
+          value: @search_state.query_param,
+          label: label,
+          remove_path: remove_path,
+          classes: 'query',
+          **@query_constraint_component_options
+        )
+      )
+      @index += 1
+    else
+      query_constraint = ''
+    end
+    query_constraint.html_safe + clause_constraints.html_safe
+  end
+
+  def clause_constraints
+    presenters = clause_presenters.to_a
+    clause_constraints = render(@facet_constraint_component.with_collection(presenters, **{index: @index, **@facet_constraint_component_options}))
+    @index += presenters.length
+    clause_constraints
+  end
+
+  def facet_constraints
+    presenters = facet_item_presenters.to_a
+    facet_constraints = render(@facet_constraint_component.with_collection(presenters, **{index: @index, **@facet_constraint_component_options}))
+    @index += presenters.length
+    facet_constraints
+  end
+
+  def render?
+    @search_state.has_constraints? || is_for_search_history?
+  end
+
+  private
+
+  def clause_presenters
+    return to_enum(:clause_presenters) unless block_given?
+
+    @search_state.clause_params.each do |key, clause|
+      field_config = helpers.blacklight_config.search_fields[clause[:field]]
+      if clause['query'].present?
+        yield Blacklight::ClausePresenter.new(key, clause, field_config, helpers)
+      else
+        next
+      end
+    end
+  end
+
+  def is_for_search_history?
+    @query_constraint_component.name == 'SearchHistoryConstraintLayoutComponent'
+  end
+end
